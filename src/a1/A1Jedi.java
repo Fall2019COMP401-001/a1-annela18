a11cf68e0fb054774953d640ba91ad6bc4845f3a
laurenfrench <laurenfrench@laurens-macbook.wireless-psk.unc.edu>
2019-08-30 13:18:30 -0400

    Edited ver

--- src/a1/A1Jedi.java
+++ src/a1/A1Jedi.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 public class A1Jedi {
 
 	public static void main(String[] args) {
-		
+
 		Scanner scan = new Scanner(System.in);
 
 		// Your code follows here.
@@ -17,46 +17,54 @@ public class A1Jedi {
 		String[] foodNameArray = new String[itemsInStore];
 		int[] foodCountArray = new int[itemsInStore];
 		int[] numberOfCustomersBought = new int[itemsInStore];
-		
+
 		for (int i = 0; i < itemsInStore; i++) {
 			String foodName = scan.next();
 			double foodPrice = scan.nextDouble();
 			foodPriceArray[i] = foodPrice;
-			foodNameArray[i] = foodName; 
+			foodNameArray[i] = foodName;
 		}
-		
+
 		int customerCount = scan.nextInt();
 		String[] nameOfCustomer = new String[customerCount];
 		for (int j = 0; j < customerCount; j++) {
-				String name = scan.next() + " " + scan.next();
-				nameOfCustomer[j] = name;
-				int itemsBought = scan.nextInt();
-		/* Up to here is good. Want a for loop that scrolls through the items bought,
-		 * and if the nameOfItem is equal to the array in the foodName array, add it
-		 * the quantityOfItem to a new array and store it in the same place as the
-		 * foodNameArray.
-		 */
-				
-				for (int k = 0; k < itemsBought; k++) {
-						int quantityOfItem = scan.nextInt();
-						String nameOfItem = scan.next();
-						for (int i = 0; i < foodNameArray.length; i++) {
-							if(foodNameArray[i].equals(nameOfItem)) {
-								foodCountArray[i] += quantityOfItem;
-								numberOfCustomersBought[i] += 1;
-							
-							}
-						}	
-				}		
+			boolean[] trueOrFalse = new boolean[foodNameArray.length];
+			for (int h = 0; h < trueOrFalse.length; h++) {
+				trueOrFalse[h] = false;
+			}
+			String name = scan.next() + " " + scan.next();
+			nameOfCustomer[j] = name;
+			int itemsBought = scan.nextInt();
+			/*
+			 * Up to here is good. Want a for loop that scrolls through the items bought,
+			 * and if the nameOfItem is equal to the array in the foodName array, add it the
+			 * quantityOfItem to a new array and store it in the same place as the
+			 * foodNameArray.
+			 */
+
+			for (int k = 0; k < itemsBought; k++) {
+				int quantityOfItem = scan.nextInt();
+				String nameOfItem = scan.next();
+				for (int i = 0; i < foodNameArray.length; i++) {
+					if (foodNameArray[i].equals(nameOfItem)) {
+						foodCountArray[i] += quantityOfItem;
+						if (!trueOrFalse[i]) {
+							numberOfCustomersBought[i] += 1;
+							trueOrFalse[i] = true;
+						} 
+					}	
+				}
+			}	
 		}
 		for (int i = 0; i < foodNameArray.length; i++) {
 			if (foodCountArray[i] == 0) {
 				System.out.println("No customers bought " + foodNameArray[i]);
 			} else {
-			System.out.println(numberOfCustomersBought[i] + " customers bought " + foodCountArray[i] + " " + foodNameArray[i]);
+				System.out.println(
+						numberOfCustomersBought[i] + " customers bought " + foodCountArray[i] + " " + foodNameArray[i]);
+			}
+			scan.close();
+
 		}
-	scan.close();
-	
-	}
 	}
 }
